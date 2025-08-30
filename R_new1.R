b74acc0cebb2a0aba0de5ac8dbcb5d8645356b49
kaviram2k <138896854+kaviram2k@users.noreply.github.com>
2025-08-30 18:32:32 +0530

    Add files via upload
    basic R

---
+++ R_new1.R
@@ -0,0 +1,78 @@
+2+2
+
+1:250
+
+print("hello wolrd")
+
+1:10
+
+a <- 1
+2 -> b
+c <- d <- e <- 3
+x <- c(1,2,5,8)
+x
+
+
+0:100
+100:0
+seq(10)
+seq(0,30, by=2)
+seq(40,10, by=-3)
+
+
+(y <- c(5,1,0,10))  # just asgin  and print
+
+x + y
+
+x*2
+
+2^3
+sqrt(64)
+log(100)
+log10(100)
+
+num <- 13
+num
+typeof(num)     # default  - double
+
+cha1 <- "A"
+typeof(cha1)
+
+logic1 <- FALSE
+logic2 <- TRUE
+
+typeof(logic1)
+typeof(logic2)
+
+
+# vector
+
+vec1 <- c(2,6,3,9)
+vec2 <- c(T,F,F,T)
+vec3 <- c("a","b","c","d")
+
+vec1
+vec2
+vec3
+
+is.vector(vec3)
+
+#matrix
+mat <- matrix(c(T,T,T,F,T,F,F,F),nrow=2)
+
+#array
+
+a1 <- array(c(1:24),c(4,3,2))
+a1
+
+
+# data frame
+
+vnum <- c(1,2,3)
+vstr <- c("A","B","C")
+vlog <- c(T,F,T)
+cf1 <-cbind(vnum,vstr,vlog)
+cf1
+cf2 <- as.data.frame(cbind(vnum,vstr,vlog))
+cf1
+cf2                     
